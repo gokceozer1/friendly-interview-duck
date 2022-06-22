bede8091d8fd425d281ded061b004be188d94092
Gokce Ozer <gokce.ozer@trivago.com>
2022-06-23 00:37:06 +0200

    create home page and global navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import React, { Component } from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
+import { Route, Routes } from "react-router-dom"
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App ">
+        <Navbar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="/questions" element={<Questions />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/questions/:id/edit" element={<QuestionEdit />} />
+              <Route path="/questions/:id" element={<Question />} />
+              <Route path="/articles/:id" element={<Article />} />
+              <Route path="/articles/:id/edit" element={<ArticleEdit />} />  */}
+            </Routes>
+          </div>
+      </div>
+    );
+  }
 }
+
+export default App;
