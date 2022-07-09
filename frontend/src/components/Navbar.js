5ab956c0cfbe664961ce61412e5b3e1674fa5494
Gokce Ozer <gokce.ozer@trivago.com>
2022-07-09 10:24:22 +0200

    fix the class warning

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function NavBar() {
                   <div className="flex items-center justify-between w-full md:w-auto">
                     <a href="/">
                       <span className="sr-only">Workflow</span>
-                      <i class="fa-solid fa-anchor fa-2xl text-indigo-600"></i>
+                      <i className="fa-solid fa-anchor fa-2xl text-indigo-600"></i>
                     </a>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
